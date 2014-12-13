73bbc9870c3aad23e2dd7dc7daa652cf04eea22a
sheenaVerghese <sheena.verghese@gmail.com>
2014-12-13 12:20:51 +0800

    Made it neater

--- Destination_Database.php
+++ Destination_Database.php
@@ -9,32 +9,32 @@
                         $password = "root";
                         $dbname = "sqm";
                         $origin=$_GET['origin'];
-                        //$pwd=isset($_POST['passport'])?$_POST['passport']:" ";
+                        
                         // Create connection
                         $conn = new mysqli($servername, $username, $password, $dbname);
                         // Check connection
                         if ($conn->connect_error) {
                              die("Connection failed: " . $conn->connect_error);
                         }
-                        echo $origin."wakak";
+                        
                         $sql = "SELECT Destination FROM flight WHERE origin='".$origin."'";
                         $result = $conn->query($sql);
+                        echo "<select>";
+                       
                         if ($result->num_rows > 0) {
                              // output data of each row
-                             echo "<select>";
+                             echo "<option value='dest' selected>Select Destination</option>";
                              while(($row = $result->fetch_assoc())) {
                                $destination=$row['Destination'];
-                                echo "<option value=".$destination.">".$destination."</option>";
-                               
-                                
+                               echo "<option value=".$destination.">".$destination."</option>";
                                 
                              }
-                             echo "</select>";
+                             
                         } 
                        else{
-                             echo "0 results";
+                             echo "<option value='not_found'>No Destinations Available</option>";
                         }
-
+                        echo "</select>";
                         $conn->close();
                 ?>  
         
